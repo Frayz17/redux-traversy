99a40f96a1b99d2cc5ce9d6d35e3c2841ad01387
Andrey <andrydmit@gmail.com>
2019-11-15 12:06:52 +0200

    update

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 
 export default class PostForm extends Component {
+  state = {
+    title: '',
+    body: ''
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
+
   render() {
+    const { title, body } = this.state;
+    const { handleChange, onSubmit } = this;
     return (
       <div>
         <h1>Add Post</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <div>
             <label>Title:</label> <br />
-            <input type='text' />
+            <input
+              type="text"
+              name="title"
+              onChange={handleChange}
+              value={title}
+            />
           </div>
+          <br />
+          <div>
+            <label>Body:</label> <br />
+            <textarea name="body" onChange={handleChange} value={body} />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
